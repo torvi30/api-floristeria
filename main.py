27f58a3fb17a85fc38e87b11640f4d16e374b473
torvi30 <victortamayopine@gmail.com>
2025-08-08 11:25:14 -0500

    Modify main.py to add redirect endpoint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
 from db import Base, engine
 from login.routes import router as login_router
 from products.routes import router as product_router
@@ -27,4 +28,6 @@ app.include_router(user_router, prefix="/api", tags=["Users"])
 app.include_router(product_router, prefix="/api", tags=["Products"])
 app.include_router(order_router, prefix="/api", tags=["Orders"])
 
-
+@app.get("/")
+async def redirect_to_docs(request: Request):
+    return RedirectResponse(url="/docs")

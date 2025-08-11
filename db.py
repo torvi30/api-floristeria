05a8d07662289b435bffc80895b577e1f4078e2f
torvi30 <victortamayopine@gmail.com>
2025-08-11 15:36:21 -0500

    Refactor database configuration to use PostgreSQL with environment variables

--- db.py
+++ db.py
@@ -1,15 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-# Configuración de la base de datos SQLite
-DATABASE_URL = "sqlite:///./floristeria.db"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}) #Estandar
+load_dotenv()  # Carga las variables del archivo .env
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
-def get_db(): # Funcion  
+def get_db():
     db = SessionLocal()
     try:
         yield db
